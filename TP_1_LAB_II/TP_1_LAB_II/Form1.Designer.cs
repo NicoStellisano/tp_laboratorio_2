f491e68ea6f7903481e8c1d93d6c5284b8bdd4e6
NicoStellisano <nicostellisano@hotmail.com>
2016-09-13 08:58:52 -0300

    Actualizacion

--- TP_1_LAB_II/TP_1_LAB_II/Form1.Designer.cs
+++ TP_1_LAB_II/TP_1_LAB_II/Form1.Designer.cs
@@ -61,9 +61,9 @@
             this.lblResultado.AutoSize = true;
             this.lblResultado.Location = new System.Drawing.Point(13, 13);
             this.lblResultado.Name = "lblResultado";
-            this.lblResultado.Size = new System.Drawing.Size(35, 13);
+            this.lblResultado.Size = new System.Drawing.Size(13, 13);
             this.lblResultado.TabIndex = 2;
-            this.lblResultado.Text = "label1";
+            this.lblResultado.Text = "0";
             // 
             // cmbOperacion
             // 
