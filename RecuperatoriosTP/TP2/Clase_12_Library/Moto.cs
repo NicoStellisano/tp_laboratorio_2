637f854bf2d69a334153095fc2c7d9654c269c08
NicoStellisano <nicostellisano@hotmail.com>
2016-10-28 17:08:46 -0300

    Recuperatorio tp2

---
+++ RecuperatoriosTP/TP2/Clase_12_Library/Moto.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clase_12_Library_2;
+using System.Drawing;
+
+namespace Clase_12_Library
+{
+    public class Moto:Vehiculo
+    {
+        /// <summary>
+        /// Crea el objeto Moto(Constructor)
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="patente"></param>
+        /// <param name="color"></param>
+        public Moto(EMarca marca, string patente, ConsoleColor color)
+            : base(patente, marca, color)
+        {
+        }
+        /// <summary>
+        /// Las motos tienen 2 ruedas
+        /// </summary>
+        public override short CantidadRuedas
+        {
+            get
+            {
+                return 2;
+            }
+
+            set
+            {
+                this.CantidadRuedas = value;
+            }
+        }
+
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("MOTO");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("RUEDAS : " + this.CantidadRuedas);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ RecuperatoriosTP/TP2/Clase_12_Library/Vehiculo.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clase_12_Library;
+
+namespace Clase_12_Library_2
+{
+    public abstract class Vehiculo
+    {
+        public enum EMarca
+        {
+            Yamaha, Chevrolet, Ford, Iveco, Scania, BMW
+        }
+
+        private EMarca _marca;
+        private string _patente;
+        private ConsoleColor _color;
+
+        /// <summary>
+        /// Retornará la cantidad de ruedas del vehículo
+        /// </summary>
+        public abstract short CantidadRuedas { get; set; }
+        /// <summary>
+        /// Crea el objeto Vehiculo (Constructor)
+        /// </summary>
+        /// <param name="patente"></param>
+        /// <param name="marca"></param>
+        /// <param name="color"></param>
+        public Vehiculo(string patente ,EMarca marca ,ConsoleColor color)
+        {
+             this._color = color;
+             this._patente = patente;
+             this._marca = marca;
+         }
+        /// <summary>
+        /// Muestra los datos del vehículo
+        /// </summary>
+        /// <returns></returns>
+         public virtual string Mostrar()
+         {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("PATENTE: {0}\r\n", this._patente);
+            sb.AppendFormat("MARCA  : {0}\r\n", this._marca.ToString());
+            sb.AppendFormat("COLOR  : {0}\r\n", this._color.ToString());
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+         }
+
+        /// <summary>
+        /// Dos vehículos son iguales si comparten la misma patente
+        /// </summary>
+         /// <param name="v1">Vehiculo1</param>
+         /// <param name="v2">Vehiculo2</param>
+        /// <returns></returns>
+        public static bool operator ==(Vehiculo v1, Vehiculo v2)
+        {
+            return (v1._patente == v2._patente);
+        }
+        /// <summary>
+        /// Dos vehículos son distintos si su patente es distinta
+        /// </summary>
+        /// <param name="v1">Vehiculo1</param>
+        /// <param name="v2">Vehiculo2</param>
+        /// <returns></returns>
+        public static bool operator !=(Vehiculo v1, Vehiculo v2)
+        {
+            return !(v1 == v2);
+        }
+
+        public override bool Equals(Object obj)
+        {
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            return 1;
+        }
+
+      
+    }
+}
