c57962c73d17f1ccc54622121318de1866434efe
NicoStellisano <nicostellisano@hotmail.com>
2016-11-28 17:35:28 -0300

    TP4

---
+++ TP4_LAB_II/Archivos/Texto.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Archivos
+{
+    public class Texto : IArchivo<string>
+    {
+        private string _ruta;
+        public Texto(string archivo)
+        {
+            this._ruta = archivo;
+        }
+
+        public bool guardar(string datos)
+        {
+            try
+            {
+                using (StreamWriter escritor = new StreamWriter(this._ruta, true)) 
+                {
+                    escritor.WriteLine(datos);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw new Exception();
+            }
+            return true;
+        }
+        
+
+        public bool leer(out List<string> datos)
+        {
+            datos = new List<string>();
+            try
+            {
+                using (StreamReader lector = new StreamReader(this._ruta))
+                {
+                    while (lector.EndOfStream == false) 
+                    {
+                        datos.Add(lector.ReadLine());
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                
+            }
+            return true;
+        }
+    }
+}
---
+++ TP4_LAB_II/Hilo/Descargador.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net; // Avisar del espacio de nombre
+using System.ComponentModel;
+
+namespace Hilo
+{
+    public delegate void Descargando(int Progreso);
+    public delegate void Descargado(string html);
+    public class Descargador
+    {
+        private string html;
+        private Uri direccion;
+
+        public Descargador(Uri direccion)
+        {
+            
+            this.direccion = direccion;
+        }
+
+        public void IniciarDescarga()
+        {
+            try
+            {
+                WebClient web = new WebClient();
+                web.DownloadProgressChanged += this.WebClientDownloadProgressChanged;
+                web.DownloadStringCompleted += this.WebClientDownloadCompleted;
+
+                web.DownloadStringAsync(this.direccion);
+                
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+        public event Descargando downloading;
+        public event Descargado terminado;
+
+        private void WebClientDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            this.downloading(e.ProgressPercentage);
+        }
+        private void WebClientDownloadCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            this.terminado(e.Result);
+        }
+    }
+}
